9af00d659af58561163029b35c31181a1803a727
Christian Engel <engel-ch@outlook.com>
2022-04-04 02:38:06 +0200

    support for multiple input files

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.17
 
 require github.com/urfave/cli/v2 v2.4.0
 
-require github.com/engelch/go_libs/v2 v2.6.0
+require github.com/engelch/go_libs/v2 v2.8.0
 
 require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
--- main.go
+++ main.go
@@ -5,12 +5,13 @@ import (
 	"encoding/hex"
 	"errors"
 	"fmt"
+	"os"
+
 	ce "github.com/engelch/go_libs/v2"
 	"github.com/urfave/cli/v2"
-	"os"
 )
 
-const appVersion = "0.4.2"
+const appVersion = "0.7.0"
 const appName = "gosha256"
 
 const _debug = "debug" // long (normal) name of CLI option
@@ -40,6 +41,24 @@ func check(e error) {
 	}
 }
 
+func calcSha256(filename string, raw bool) {
+	dat, err := os.ReadFile(filename)
+	check(err)
+	h := sha256.New()
+	h.Write(dat)
+	sha256_hash := (h.Sum(nil))
+	ce.CondDebugln("SHA256 is:", hex.EncodeToString(sha256_hash))
+	if raw {
+		_, _ = os.Stdout.Write(sha256_hash)
+	} else { // pretty print stdin without filename
+		if filename != "/dev/stdin" {
+			fmt.Printf("%x %s\n", sha256_hash, filename)
+		} else {
+			fmt.Printf("%x\n", sha256_hash)
+		}
+	}
+}
+
 // main start routine
 func main() {
 	var portNumber uint = 65536 // set to non acceptable value, if unchanged then a CLI option was missing => error&exit
@@ -50,29 +69,20 @@ func main() {
 	app.Flags = commandLineOptions(&portNumber, &privKeyFile, &publicKeyFile)
 	app.Name = appName
 	app.Version = appVersion
-	app.Usage = appName + " [--raw | -r] [--debug | -d] [<<file>>]\n\n\tIf no file is given, then read from stdin.\n\t" +
+	app.Usage = appName + " [--raw | -r] [--debug | -d] [<<file>>]...\n\n\tIf no file is given, then read from stdin.\n\t" +
 		"The SHA-256 is output as hex encoded by default.\n\tUsing, raw, it is output as a 32 byte sequence."
 
 	app.Action = func(c *cli.Context) error {
-		var filename string
 		switch c.NArg() {
-		case 1:
-			filename = c.Args().Get(0)
 		case 0:
-			filename = "/dev/stdin"
+			calcSha256("/dev/stdin", c.Bool(_raw))
 		default:
-			return errors.New("Must be called with 1 arg")
-		}
-		dat, err := os.ReadFile(filename)
-		check(err)
-		h := sha256.New()
-		h.Write(dat)
-		sha256_hash := (h.Sum(nil))
-		ce.CondDebugln("SHA256 is:", hex.EncodeToString(sha256_hash))
-		if c.Bool(_raw) {
-			_, _ = os.Stdout.Write(sha256_hash)
-		} else {
-			fmt.Printf("%x\n", sha256_hash)
+			if c.Bool(_raw) {
+				return errors.New("Multiple files digest calculation with raw output not supported.")
+			}
+			for index, _ := range make([]int, c.NArg()) {
+				calcSha256(c.Args().Get(index), c.Bool(_raw))
+			}
 		}
 		return nil
 	}
